432462137085c317174bcdf85ea5ee2f57c8b3c1
BriloJune <ssoung04@gmail.com>
2024-12-09 18:01:10 +0900

    baekjoon: 2231
    백준 2231: 분해합
    BruteFore

---
+++ BruteForce_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream> 
+
+using namespace std;
+
+int solution(int N) {
+	int num = 0;
+	int sum = 0;
+	for (int i = 0; i <= N; i++) {
+		sum = i;
+		num = i;
+		while (num != 0 && N >= sum) {
+			sum += num % 10;
+			num /= 10;
+		}
+		if (sum == N) {
+			return i;
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << solution(n);
+}
